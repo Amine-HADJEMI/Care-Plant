cf4b74bde8226069afc0cece680553e239a4c543
Vladyslav Dokounin <dyacenkonikita@gmail.com>
2023-04-18 12:02:42 +0200

    merge

--- back/src/models/database.js
+++ back/src/models/database.js
@@ -1,19 +1,5 @@
 const { Sequelize, DataTypes } = require('sequelize');
 
-<<<<<<< HEAD
-const Database = {};
-
-Database.db = new sqlite3.Database(
-  "database.db",
-  sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE,
-  (err) => {
-    if (err) {
-      console.error(err.message);
-    }
-    console.log("Connected to the database.");
-  }
-);
-=======
 const sequelize = new Sequelize({
   dialect: 'sqlite',
   storage: 'database.sqlite'
@@ -38,7 +24,6 @@ const User = sequelize.define('user', {
     allowNull: false
   }
 });
->>>>>>> d1b206e04c839c4b555dfb5f3de34f2fbd6a0899
 
 const Message = sequelize.define('message', {
   text: {
@@ -55,36 +40,6 @@ const Message = sequelize.define('message', {
   }
 });
 
-<<<<<<< HEAD
-Database.db.run(`CREATE TABLE users_backup AS SELECT * FROM users;`);
-
-Database.db.run(`
-    CREATE TABLE IF NOT EXISTS messages (
-        _id INTEGER PRIMARY KEY AUTOINCREMENT,
-        text TEXT NOT NULL,
-        user TEXT NOT NULL,
-        createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    );`);
-Database.db.run(`
-    CREATE TABLE IF NOT EXISTS password_reset_codes (
-        email VARCHAR(255) NOT NULL,
-        code VARCHAR(255) NOT NULL,
-        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-        PRIMARY KEY (email)
-    );`);
-
-Database.db.run(`CREATE TABLE IF NOT EXISTS posts (
-    id INTEGER PRIMARY KEY,
-    title TEXT NOT NULL,
-    description TEXT NOT NULL,
-    image BLOB,
-    userName TEXT,
-    createdAt DATETIME NOT NULL,
-    carePlant INTEGER NOT NULL DEFAULT 0 CHECK (carePlant IN (0, 1))
-)`);
-
-module.exports = Database;
-=======
 const PasswordResetCode = sequelize.define('password_reset_code', {
     email: {
       type: Sequelize.DataTypes.STRING(255),
@@ -137,4 +92,3 @@ const Post = sequelize.define('post', {
 sequelize.sync({ force: true }); 
 
 module.exports = { User, Message, Post, PasswordResetCode }; 
->>>>>>> d1b206e04c839c4b555dfb5f3de34f2fbd6a0899
