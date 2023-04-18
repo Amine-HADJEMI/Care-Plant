94fd047cea463caeb9d81efe32d55dac4528a209
Vladyslav Dokounin <dyacenkonikita@gmail.com>
2023-04-18 16:41:38 +0200

    ajout role 'User' par default

--- back/src/controllers/userController.js
+++ back/src/controllers/userController.js
@@ -1,13 +1,12 @@
 const bcrypt = require("bcrypt");
 const Database = require("../models/database");
-const Status = require("../utils/status")
-const  {User, sequelize} = require('../models/database'); 
-const {  Op  } = require('sequelize');
-
+const Status = require("../utils/status");
+const { User, Role, sequelize } = require("../models/database");
+const { Op } = require("sequelize");
 // const db = Database.db
 
 sequelize.sync().then(() => {
-  console.log('Models synchronized with database in userController');
+  console.log("Models synchronized with database in userController");
 });
 
 async function getAllUsers(req, res) {
@@ -22,12 +21,15 @@ async function getAllUsers(req, res) {
 
 async function createUser(req, res) {
   try {
-
     const password = req.body.password;
-    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\w\s]).{8,20}$/;
-    
+    const passwordRegex =
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\w\s]).{8,20}$/;
+
     if (!passwordRegex.test(password)) {
-      res.status(400).send({ message: 'Invalid password', status: Status.INVALID_PASSWORD_FORMAT });
+      res.status(400).send({
+        message: "Invalid password",
+        status: Status.INVALID_PASSWORD_FORMAT,
+      });
     } else {
       const saltRounds = 10;
       const hash = await bcrypt.hash(password, saltRounds);
@@ -37,34 +39,42 @@ async function createUser(req, res) {
           where: {
             [Op.or]: [
               { userName: req.body.userName },
-              { email: req.body.email }
-            ]
-          }
+              { email: req.body.email },
+            ],
+          },
         });
-        
-  
+
         if (existingUsers.length > 0) {
-          res.status(200).send({ message: 'User already exists', status: Status.USER_ALREADY_EXISTS });
-        }
-        else {
+          res.status(200).send({
+            message: "User already exists",
+            status: Status.USER_ALREADY_EXISTS,
+          });
+        } else {
           const user = await User.create({
             userName: req.body.userName,
             name: req.body.name,
             email: req.body.email.toLowerCase(),
-            password: hash
+            password: hash,
           });
-          
-          res.status(201).send({ message:'User created successfully', status: Status.CREATE_USER});
+          // Add default role "User" to user
+          const role = await Role.findOne({ where: { name: "User" } });
+          await user.setRole(role);
+
+          res.status(201).send({
+            message: "User created successfully",
+            status: Status.CREATE_USER,
+          });
         }
       } else {
-        res.status(200).send({ message:'Please complete the data', status: Status.INCOMPELETE_DATA });
+        res.status(200).send({
+          message: "Please complete the data",
+          status: Status.INCOMPELETE_DATA,
+        });
       }
     }
-
-
   } catch (error) {
     console.error(error);
-    res.status(500).send('Error creating user');
+    res.status(500).send("Error creating user");
   }
 }
 
@@ -72,37 +82,37 @@ async function updateUser(req, res) {
   const { name, email, password } = req.body;
   const { userName } = req.params;
   try {
-    await User.update(
-      { name, email, password },
-      { where: { userName } }
-    );
+    await User.update({ name, email, password }, { where: { userName } });
     res.json({ message: "User updated successfully" });
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: "Error updating user" });
   }
-  
-};
+}
 
-async function deleteUser(req, res) {  
+async function deleteUser(req, res) {
   try {
-    const existingUser = await User.findOne({ where: { userName: req.params.userName }});
+    const existingUser = await User.findOne({
+      where: { userName: req.params.userName },
+    });
 
     if (existingUser.length === 0) {
-      return res.status(404).send(`The user with userName ${req.params.userName} does not exist`);
+      return res
+        .status(404)
+        .send(`The user with userName ${req.params.userName} does not exist`);
     }
 
     await User.destroy({
       where: {
-        userName: req.params.userName
-      }
+        userName: req.params.userName,
+      },
     });
-      res.status(200).send(`User with userName ${req.params.userName} deleted`);
+    res.status(200).send(`User with userName ${req.params.userName} deleted`);
   } catch (err) {
     console.error(err);
     res.status(500).send(err);
   }
-};
+}
 
 module.exports = {
   getAllUsers,
